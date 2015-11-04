7fef8f08ae51950edafd3026e1c2d79836c83afe
kazaredw <kazaredw@gmail.com>
2015-11-04 21:08:16 +0300

    Тренировка

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
+	def show
+	  @item = Item.find(params[:id])
+        end
+	
+	
 	def new
-		@item=Item.new
+	  @item=Item.new
 	end
+	def create
+	  @item=Item.new(item_params)
+	  @item.save
+	  redirect_to @item
+	end
+  private
+  	def item_params
+    	  params.require(:item).permit(:name, :description, :price, :weight)
+  	end
+
+
+
 end
